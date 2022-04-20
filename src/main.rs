72db4d81f3e1f88b5ede98e9d5e38466d11e90d3
Yann Cardon <ycardon@gmail.com>
2022-04-21 01:15:11 +0200

    fix: one file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use actix_web::{middleware, App, HttpServer};
+use actix_web::{get, post, web, middleware, App, HttpServer, HttpResponse, Responder, Result};
+use serde::Serialize;
 use std::{env, io};
 
-mod display;
+// --- HTTP SERVER ---
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
@@ -10,15 +11,60 @@ async fn main() -> io::Result<()> {
 
     HttpServer::new(|| {
         App::new()
-            // enable logger - always register actix-web Logger middleware last
             .wrap(middleware::Logger::default())
-            // register HTTP requests handlers
-            .service(display::get)
-            .service(display::post)
+            .service(get_cpu)
+            .service(get_power)
+            .service(get_display)
+            .service(post_display)
     })
     .bind("0.0.0.0:3030")?
     .run()
     .await
 }
 
-// https://hub.qovery.com/guides/tutorial/create-a-blazingly-fast-api-in-rust-part-1/
+// --- CPU ---
+
+#[derive(Serialize)]
+#[allow(non_snake_case)]
+struct CpuResponse {
+    isOnAC: bool,
+    isOnBattery: bool,
+    isCharged: bool,
+    chargingStatus: String,
+    chargePercent: u16,
+    remainingChargeTime: String,
+    message: String,
+}
+
+#[get("/cpu")]
+async fn get_cpu() -> Result<impl Responder> {
+    let cpu = CpuResponse {
+        isOnAC: true,
+        isOnBattery: false,
+        isCharged: false,
+        chargingStatus: String::from("test"),
+        chargePercent: 50,
+        remainingChargeTime: String::from("test"),
+        message: String::from("test"),
+    };
+    Ok(web::Json(cpu))
+}
+
+// --- POWER ---
+
+#[get("/power")]
+async fn get_power() -> impl Responder {
+    HttpResponse::Ok().body("GET /power")
+}
+
+// --- DISPLAY ---
+
+#[get("/display")]
+async fn get_display() -> impl Responder {
+    HttpResponse::Ok().body("GET /display")
+}
+
+#[post("/display")]
+async fn post_display() -> impl Responder {
+    HttpResponse::Ok().body("POST /display")
+}
